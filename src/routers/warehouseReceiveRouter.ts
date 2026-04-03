3e6aa43340ddd5a8422343ce59003c97ebb77edf
tuanmq <tuanmqse170562@fpt.edu.vn>
2026-04-03 08:52:56 +0700

    Reformatting to make the code more concise

--- src/routers/warehouseReceiveRouter.ts
+++ src/routers/warehouseReceiveRouter.ts
@@ -7,16 +7,8 @@ const router = Router();
 router.use(jwtMiddleware);
 
 router.get('/', requireRole(1, 2, 3), warehouseReceiveController.getAll);
-router.get(
-  '/transfer/:transferId',
-  requireRole(1, 2, 3),
-  warehouseReceiveController.getByTransferId
-);
-router.get(
-  '/received-by-suggestions',
-  requireRole(1, 2, 3),
-  warehouseReceiveController.searchReceivedBySuggestions
-);
+router.get('/transfer/:transferId', requireRole(1, 2, 3), warehouseReceiveController.getByTransferId);
+router.get('/received-by-suggestions', requireRole(1, 2, 3), warehouseReceiveController.searchReceivedBySuggestions);
 router.get('/:id', requireRole(1, 2, 3), warehouseReceiveController.getById);
 router.post('/', requireRole(2, 3), warehouseReceiveController.create);
 
